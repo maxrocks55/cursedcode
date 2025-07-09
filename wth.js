214476553d05a86fc5b358696bb8e8cf7e4e6e32
max <mlhader@outlook.com>
2025-07-08 23:24:45 -0400

    Create wth.js

---
+++ wth.js
@@ -0,0 +1,5 @@
+async dfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff() => {
+  return(`${`${`${`${`${`${`${`${`${`${`${`${"E"}`}`}`}`}`}`}`}`}`}`}`}`);
+}
+
+document.write(`${`${`${`${dfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff()}`}`}`}`);
