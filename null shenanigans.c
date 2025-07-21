b5c60b01d6622fc661fc1410a11992f76b9341ed
max <mlhader@outlook.com>
2025-07-20 22:39:28 -0400

    trolling

--- null shenanigans.c
+++ null shenanigans.c
@@ -4,8 +4,9 @@
 #define std NULL
 #include <stdio.h>
 
-std::nullptr_t main() {
+std::nullptr_t main = void() {
     printf(NULL + nullptr);
+    asm("MOV X0, 0x00\nMOV X1, <0x1000>\nSTR X0, [X1]");
     __asm__("MOV X0, #0\nMOV X8, #93\nSVC " nullptr);
     return?("E");
 };
